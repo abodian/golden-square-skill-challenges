bfe038ca3627cdf658d8a3931cf66c7e24099edd
abodian <ajnbodian@gmail.com>
2022-12-07 13:38:31 +0000

    updated check_grammar_spec file

--- spec/check_grammar_spec.rb
+++ spec/check_grammar_spec.rb
@@ -1,6 +1,6 @@
 require 'check_grammar'
 
-describe ' check_grammar method' do 
+describe 'check_grammar method' do 
   it "returns false when no sentence" do 
     expect(check_grammar("")).to eq false
   end
